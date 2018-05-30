054f499d634a955a144d3455d50c7788bacc2b7b
Matthias Bogaert <hello@matthiasbogaert.be>
2018-05-30 09:58:49 +0200

    Eerste screen gemaakt

---
+++ FactuurOverzicht/FactuurViewController.swift
@@ -0,0 +1,26 @@
+//
+//  FactuurViewController.swift
+//  FactuurOverzicht
+//
+//  Created by De Winter Laura on 30/05/18.
+//  Copyright © 2018 Comflow. All rights reserved.
+//
+
+import UIKit
+
+class FactuurViewController: UIViewController {
+    @IBOutlet weak var Titel: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func FotosTapped(_ sender: Any) {
+    }
+    
+    @IBAction func CameraTapped(_ sender: Any) {
+    }
+}
